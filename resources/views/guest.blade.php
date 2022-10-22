147c6e98655fc39e39298caccbc5320245c0be9b
Jihad Maulana Akbar <jihad.project@gmail.com>
2022-10-22 22:16:28 +0700

    add onesignal library

---
+++ resources/views/guest.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.guest_app')
+
+@section('content-title', 'DummyTitle')
+
+@section('content')
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
+    @component(config('generator.view_component').'components.panel')
+      @slot('title')
+        Title
+      @endslot
+
+      @if (session('status'))
+      <div class="alert alert-success">
+        {{ session('status') }}
+      </div>
+      @endif
+      Content goes here
+
+    @endcomponent
+  </div>
+</div>
+@endsection
